f3c96451da929c8a36d83885f2efca68063d58fa
Ben Cheng <ben.cheng@microsoft.com>
2020-07-11 15:25:19 -0400

    initial load

---
+++ databases/customerXdb.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [customerxdb].[dbo].Customer (
+    customerID int PRIMARY KEY,
+    cusotmerName varchar(50) NOT NULL
+);
+
+INSERT [customerxdb].[dbo].customer VALUES (1, 'Bob Smith');
+INSERT [customerxdb].[dbo].customer VALUES (2, 'Sam Jones');
+INSERT [customerxdb].[dbo].customer VALUES (3, 'Mike Jordan');
+INSERT [customerxdb].[dbo].customer VALUES (4, 'Magic Johnson');
+INSERT [customerxdb].[dbo].customer VALUES (5, 'JL Picard');
---
+++ databases/customerYdb.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [customerydb].[dbo].Customer (
+    customerID int PRIMARY KEY,
+    cusotmerName varchar(50) NOT NULL
+);
+
+INSERT [customerydb].[dbo].customer VALUES (1, 'Jane Summers');
+INSERT [customerydb].[dbo].customer VALUES (2, 'Samatha James');
+INSERT [customerydb].[dbo].customer VALUES (3, 'Michelle Jordan');
+INSERT [customerydb].[dbo].customer VALUES (4, 'Mary Johnson');
+INSERT [customerydb].[dbo].customer VALUES (5, 'Jenny Picard');
